0481ddd6f8bc1ce0831c253b258b7e5fe2e045d9
gardenk <jwnnekim@gmail.com>
2024-06-13 16:10:29 +0900

    refactor: extract calc_length_score method

--- similarity_checker.py
+++ similarity_checker.py
@@ -6,6 +6,9 @@ class SimilarityCheck:
             raise TypeError
 
         if len(A) >= len(B):
-            return max(60 - 60*(len(A) - len(B))/len(B), 0)
+            return self.calc_length_score(len(A), len(B))
         else:
-            return max(60 - 60 * (len(B) - len(A)) / len(A), 0)
+            return self.calc_length_score(len(B), len(A))
+
+    def calc_length_score(self, A_len, B_len):
+        return max(60 - 60 * (A_len - B_len) / B_len, 0)
